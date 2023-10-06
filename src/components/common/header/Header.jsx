d71e8f84dc5f931b8587455cf1d36a8449eab0e2
CasleyDon1 <Casleynkosi@gmail.com>
2023-10-06 17:13:41 +0200

    finished the header

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+const Header = () => {
+   return (
+     <>
+       <div className="container flex">
+         <div className="logo">
+	    <img src="./images/logo.png" alt="" />
+	 </div>
+       </div>
+     </>
+   );
+ }
+
+export default Header;
